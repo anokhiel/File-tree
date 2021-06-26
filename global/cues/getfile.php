6eb06259ea3276c5bcd74336b4217feaf7c5b6f0
Alexander Popov <alexandpopov@yandex.ru>
2021-06-26 18:56:29 +0300

    getfile fixed

--- global/cues/getfile.php
+++ global/cues/getfile.php
@@ -1,6 +1,11 @@
 <?php
 if(file_exists($_POST['file'])){
-    echo(file_get_contents('../'.substr($_POST['file'], strrpos($_POST['file'],'/')+1 )));
+    $curdir=substr(__DIR__,0,  strrpos(__DIR__,'/'));
+    $curname=substr($curdir,strrpos($curdir,'/')+1);
+    $pardir=substr($_POST['file'], strrpos($_POST['file'], $curname));
+    $fileplace=str_replace($curname,'..', $pardir);
+          echo(file_get_contents($fileplace));
+    //echo('../'.substr($_POST['file'], strpos(substr(__DIR__,  strrpos(__DIR__,'/') )) ));
     
 }else{
     echo('Файл не найден или не может быть воспроизведен');
